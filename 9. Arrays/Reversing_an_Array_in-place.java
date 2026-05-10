1c1286e9998449758328e10b6eb4eec4b2a55f41
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-10 13:05:09 +0530

    Create Reversing_an_Array_in-place.java

---
+++ 9. Arrays/Reversing_an_Array_in-place.java
@@ -0,0 +1,33 @@
+// Count how many elements are greater than a given number k.
+import java.util.*;
+class Main{
+    public static int reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while (start<end){
+            arr[start]=arr[start]^arr[end];
+            arr[end]=arr[start]^arr[end];
+            arr[start]=arr[start]^arr[end];
+            start++;
+            end--;
+        }
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+        return 0;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[3];
+        
+        arr=new int[]{2,45,14};
+        
+        System.out.println(arr[0]);
+        reverse(arr);
+        System.out.println();      // for the next line
+        System.out.println(arr[0]);
+        
+        
+        
+    }
+}
