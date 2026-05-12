befe10ee19ac1300f1a25e541bc70d05bb28696d
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-13 00:02:30 +0530

    Implement array rotation using reverse method

---
+++ Array_Problems_Java/2_LC189_Rotate_Array.java
@@ -0,0 +1,38 @@
+class Solution {
+    // Optimise approach
+    public void reverse(int[] nums, int start, int end){
+        while (start<end){
+            nums[start]=nums[start]^nums[end];
+            nums[end]=nums[start]^nums[end];
+            nums[start]=nums[start]^nums[end];
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        // handle large input.when (k>n)
+        k=k%n;
+
+        reverse(nums, 0, n-1);
+        
+        reverse(nums, 0, k-1);
+        
+        reverse(nums, k, n-1);
+
+        // Brute-Force Approach
+        // -------------------------
+        // for(int i=0;i<k;i++){
+        //     int last=nums[n-1];
+
+        //     // Right Shift of the other elements.
+        //     for(int j=n-1;j>0;j--){
+        //         nums[j]=nums[j-1];
+        //     }
+
+        //     // Move the last element to the front 
+        //     nums[0]=last;
+
+        
+    }
+}
