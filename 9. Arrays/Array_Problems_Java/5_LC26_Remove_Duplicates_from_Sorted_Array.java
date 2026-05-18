a088566c27a3bff0f5b3f3576321b7fb82af784c
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-18 22:23:39 +0530

    Update 5_LC26_Remove_Duplicates_from_Sorted_Array.java

--- 9. Arrays/Array_Problems_Java/5_LC26_Remove_Duplicates_from_Sorted_Array.java
+++ 9. Arrays/Array_Problems_Java/5_LC26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,3 +1,8 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums.length==0){
