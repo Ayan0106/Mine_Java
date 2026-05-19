618b77d79a9898e1cbcfc05c75a47cb194fb6683
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-19 23:35:25 +0530

    Update 7_Missing_Number.java

--- 9. Arrays/Intermediate thinking/7_Missing_Number.java
+++ 9. Arrays/Intermediate thinking/7_Missing_Number.java
@@ -1,1 +1,18 @@
+// Missing Number
 
+class Main {
+    public static int missingnumber(int[] arr){
+        int n=arr.length;
+        int expectedsum=n*(n+1)/2;
+        int actualsum=0;
+        for(int i=0;i<n;i++){
+            actualsum+=arr[i];
+        }
+        return expectedsum-actualsum;
+    }
+    public static void main(String[] args) {
+        int[] arr={0,1,3,4,5};
+        System.out.println("Missing Number "+missingnumber(arr));
+        
+    }
+}
