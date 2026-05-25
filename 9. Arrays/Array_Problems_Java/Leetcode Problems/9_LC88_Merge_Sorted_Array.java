c42a06caf2d2f50e3cab36c19cfa19428732f213
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-26 01:05:50 +0530

    Update 9_LC88_Merge_Sorted_Array.java

--- 9. Arrays/Array_Problems_Java/Leetcode Problems/9_LC88_Merge_Sorted_Array.java
+++ 9. Arrays/Array_Problems_Java/Leetcode Problems/9_LC88_Merge_Sorted_Array.java
@@ -1,6 +1,6 @@
 // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 ... 0s are just placeholder empty spaces in nums1.
 // Output: [1,2,2,3,5,6]
-
+// (Backward Traversing Because we have to store the sorted items in one existing Array w/o using any new Array).
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i=m-1;
