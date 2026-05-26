55f2339b26b2087465a71fed6736fbf7680acb92
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-05-27 01:07:32 +0530

    Refine comment for sortColors method
    Updated comment to clarify the Dutch National Flag Problem.

--- 9. Arrays/Array_Problems_Java/Leetcode Problems/11_LC75. Sort Colors.java
+++ 9. Arrays/Array_Problems_Java/Leetcode Problems/11_LC75. Sort Colors.java
@@ -1,4 +1,9 @@
-// THREE- POINTER APPROACH--- Dutch National Flag Problem.
+// THREE- POINTER APPROACH--- (Dutch National Flag Problem).
+
+// Example 1: Input: nums = [2,0,2,1,1,0], Output: [0,0,1,1,2,2]
+// Example 2: Input: nums = [2,0,1], Output: [0,1,2]
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
     public void sortColors(int[] nums) {
         int low=0, mid=0, high=nums.length-1;
 
